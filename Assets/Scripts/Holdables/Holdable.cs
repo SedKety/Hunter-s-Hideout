1dd775196014f501e0dece8255cae2f9472f9fdf
SedKety <nobe.mol@gmail.com>
2024-11-20 10:59:58 +0100

    hele project weg

--- Assets/Scripts/Holdables/Holdable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Holdable : MonoBehaviour
-{
-    //this method is called when the item is been used
-    public abstract void OnHeldItemUse();
-}
